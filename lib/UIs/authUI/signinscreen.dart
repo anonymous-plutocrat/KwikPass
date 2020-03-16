a9d97bb042bfb30f8118153b6673dc3649bf8292
Denzel <chiagozieani145@gmail.com>
2020-03-16 21:53:26 +0100

    Jeez work already this textformfield

--- lib/UIs/authUI/signinscreen.dart
+++ lib/UIs/authUI/signinscreen.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_spinkit/flutter_spinkit.dart';
+// import 'package:road_to_flutter/UIs/authUI/signupscreen.dart';
+// import 'package:road_to_flutter/UIs/homescreen.dart';
+import 'package:road_to_flutter/auth/auths.dart';
 import 'package:road_to_flutter/shared/inputDecor.dart';
+
 
 
 class SignIn extends StatefulWidget {
@@ -11,41 +17,97 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
 
+    Auths _auths = Auths();
     final _formKey = GlobalKey<FormState>(); 
-    String username = '';
+    String email = '';
     String password = '';
+    String error = '';
 
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.pinkAccent,
         title: Text('Sign In'),
         elevation: 0,
       ),
-          body: Form(
+      body: Form(
         key: _formKey,
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 20,),
-            TextFormField(
-              decoration: inputDecor.copyWith(hintText:'UserName'),
-              validator: (value) => value.isEmpty ? 'Field Cannot be Empty' : null,
-              onChanged: (value){
-                setState(() => username = value);
-              },
-            ),
-             SizedBox(height: 20,),
-            TextFormField(
-              decoration: inputDecor.copyWith(hintText:'Password'),
-              obscureText: true,
-              validator: (value) => value.length < 8 ? 'Password must be 8 characters long' : null,
-              onChanged: (value){
-                setState(() => password = value);
-              }             
-              ,
-            ),
-            SizedBox(height: 30,),
-            FlatButton(child: Text('SignIn'), onPressed: (){},color: Colors.pinkAccent,)
-          ],
+        child: ListView(
+            children: <Widget> [ 
+            Column(
+            children: <Widget>[
+              SizedBox(height: 20,),
+                Center(
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: inputDecor.copyWith(hintText:'Email',hintStyle: TextStyle(fontSize: 12)),
+                    validator: (value){
+                      if(value.isEmpty){
+                        return 'Field cannot be empty';
+                      }
+                      return null;
+                    },
+                    onChanged: (value){
+                      setState(() => email = value);
+                    },
+                  ),
+                ),
+               SizedBox(height: 20,),
+                Center(
+                  child: TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    obscureText: true,
+                    decoration: inputDecor.copyWith(hintText:'Password',hintStyle: TextStyle(fontSize: 12)),
+                    validator: (value){
+                      if(value.isEmpty){
+                        return 'Field cannot be empty';
+                      }
+                      return null;
+                    },
+                    onChanged: (value){
+                      setState(() => password = value);
+                    },
+                  ),
+                ),
+              SizedBox(height: 30,),
+              FlatButton(
+                child: Text('SignIn'), 
+                onPressed: ()async {
+                  if(_formKey.currentState.validate()){
+                     var result = await _auths.signInUser(email, password);
+                     if(result == null){
+                       setState(() {
+                         error = 'Invalid credentials please correct them';
+                       });
+                     }
+                    //  return HomeScreen();
+                  }
+                  // return SpinKitChasingDots(size: 70, color: Colors.pink,);
+                },
+              color: Colors.pinkAccent,),
+              SizedBox(height: 10,),
+              Text(error,style: TextStyle(color: Colors.red),),
+              SizedBox(height: 10,),
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: TextStyle(color: Colors.black),
+                  children:<TextSpan>[
+                    TextSpan(
+                      text: 'Sign up', 
+                      style: TextStyle(color: Colors.pinkAccent),
+                      recognizer: TapGestureRecognizer()
+                      ..onTap = (){
+                        print('object');
+                        // Navigator.of(context).push(MaterialPageRoute(builder: (context){ return SignUp();}));
+                      }
+                      ),
+                  ]
+                  ),
+                ),
+            ],
+          ),
+         ],
         )
       ),
     );
--- lib/UIs/authUI/signupscreen.dart
+++ lib/UIs/authUI/signupscreen.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:road_to_flutter/UIs/authUI/signinscreen.dart';
+// import 'package:road_to_flutter/UIs/homescreen.dart';
+import 'package:road_to_flutter/auth/auths.dart';
 import 'package:road_to_flutter/shared/inputDecor.dart';
+
+
+
 
 
 class SignUp extends StatefulWidget {
@@ -9,7 +17,7 @@ class SignUp extends StatefulWidget {
 
 class _SignUpState extends State<SignUp> {
 
-
+  Auths _auths = Auths();
   final _formKey = GlobalKey<FormState>();
   String firstName = '';
   String lastName = '';
@@ -21,6 +29,7 @@ class _SignUpState extends State<SignUp> {
   String city = '';
   String phoneNo = '';
   String email = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +62,7 @@ class _SignUpState extends State<SignUp> {
               ),
               SizedBox(height: 20,),
               TextFormField(
-                decoration: inputDecor.copyWith(hintText:'LastName',hintStyle: TextStyle(fontSize: 12)),
+                decoration: inputDecor.copyWith(hintText:'Email',hintStyle: TextStyle(fontSize: 12)),
                 validator: (value){
                   if(value.isEmpty){
                     return 'Field cannot be empty';
@@ -66,7 +75,7 @@ class _SignUpState extends State<SignUp> {
               ),
               SizedBox(height: 20,),
                 TextFormField(
-                decoration: inputDecor.copyWith(hintText:'Email',hintStyle: TextStyle(fontSize: 12)),
+                decoration: inputDecor.copyWith(hintText:'LastName',hintStyle: TextStyle(fontSize: 12)),
                 validator: (value){
                   if(value.isEmpty){
                     return 'Field cannot be empty';
@@ -172,7 +181,41 @@ class _SignUpState extends State<SignUp> {
                 },
               ),
               SizedBox(height: 20,),
-              RaisedButton(child: Text('SignUp'), onPressed: (){},color: Colors.pinkAccent,elevation: 0.1,),
+              SizedBox(height: 10,),
+              Text(error,style: TextStyle(color: Colors.red),),
+              RaisedButton(
+                child: Text('SignUp'),
+                onPressed: ()async{
+                  if(_formKey.currentState.validate()){
+                    var result = await _auths.signUpUser(email, password);
+                    if(result == null){
+                      setState(() {
+                        error = 'Invalid credentials please correct them';
+                        // Navigator.of(context).push(MaterialPageRoute(builder: (context){ return HomeScreen();}));
+                      });
+                    }
+                    // return HomeScreen();
+                  }
+                  // return SpinKitChasingDots(size: 70, color: Colors.pink,);
+                },
+                color: Colors.pinkAccent,elevation: 0.1,),
+            SizedBox(height: 10,),
+            RichText(
+              text: TextSpan(
+                text: 'Have an account? ',
+                style: TextStyle(color: Colors.black),
+                children:<TextSpan>[
+                  TextSpan(
+                    text: 'Sign in', 
+                    style: TextStyle(color: Colors.pinkAccent),
+                    recognizer: TapGestureRecognizer()
+                    ..onTap = (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context){ return SignIn();}));
+                      }
+                    ),
+                  ]
+                ),
+              ),
              ]
            ),
          ],
